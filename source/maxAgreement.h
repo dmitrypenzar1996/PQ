1696f706d7924d097bb5a549105a7fa4dfebed36
AndrewSigorskih <crescent8547@yandex.ru>
2017-10-16 01:07:32 +0300

    Update maxAgreement.h

--- source/maxAgreement.h
+++ source/maxAgreement.h
@@ -1,22 +1,26 @@
-#ifndef _MAXAGREEMENT_H_
-#define _MAXAGREEMENT_H_
+#ifndef _MAX_AGREEMENT_H_
+#define _MAX_AGREEMENT_H_
 #include "Tree.h"
 #include "add.h"
 #include "consensus.h"
-#include "growTree.h"
 #include <stdlib.h>
-#include <assert.h>
 #include <string.h>
 
-BranchArray* treeToBranchModified(Tree* tree, int* permutation);
+BranchArray* treeRootedToBranchArray(Tree* tree, int* permutation);
+void stackCycle(Node* node, NodeStack* stack);
 Tree* deleteLeaves(Tree* tree, char** leavesToDelete, int leavesToDeleteAmount);
-void treesPrune(Tree* tree1, Tree* tree2);
-unsigned* treeTopSort(Tree* tree);
+Tree** treesPrune(Tree* tree1, Tree* tree2);
+Tree* treeRoot(Tree* tree, unsigned nodeID, unsigned neighbourID, char newTree);
+Tree* treeUnRoot(Tree* tree, char newTree);
+unsigned* treeRootAndTopSort(Tree* tree, unsigned nodeID, unsigned neighbourID, unsigned* setPermutation);
 int* calculateLeavesPermutation(Tree* tree1, Tree* tree2);
-Tree* oneLeafTreeCreate(char* name);
-int* countVariants(Tree*** TAB, int a, int w, int b, int c, int x, int y);
-Tree* merge2w3(Tree* tree1, Tree* tree2);
-Tree* merge3w3(Tree* tree1, Tree* tree2);
-Tree* mergeTrees(Tree* tree1, Tree* tree2);
-void MAST(Tree* tree1, Tree* tree2);
+int branchGetLeavesPosNum(Branch* br);
+int* countVariants(Branch*** TAB, int a, int w, int b, int c, int x, int y);
+Branch* branchOR(Branch* br1, Branch* br2);
+int* getTreeLeavesPos(Tree* tree);
+unsigned* branchToLeavesArr(Branch* br, unsigned leavesNum);
+int** getAllRoots(Tree* tree);
+Tree* makeMAST(Branch* br, Tree* tree1);
+Branch* MAST(Tree* intree1, Tree* intree2, unsigned* set1, unsigned* set2, unsigned* setPermutation1, unsigned* setPermutation2);
+Tree* UMAST(Tree* tree1, Tree* tree2);
 #endif
